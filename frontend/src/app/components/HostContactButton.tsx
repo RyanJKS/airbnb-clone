5274c2600d77abdcc9c8b920f3add8321dda1231
RyanJKS <rjhelan@gmail.com>
2024-07-22 21:43:34 +0100

    Add reservation page

---
+++ frontend/src/app/components/HostContactButton.tsx
@@ -0,0 +1,11 @@
+
+
+const HostContactButton = () => {
+    return (
+        <div className="mt-4 py-4 px-6 bg-airbnb text-white rounded-xl cursor-pointer hover:bg-airbnb-dark transition">
+            Contact
+        </div>
+    )
+}
+
+export default HostContactButton
