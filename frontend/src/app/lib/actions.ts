6a29973c4cd1c754b2d4a06f67b3b94c9d6ca2fa
RyanJKS <rjhelan@gmail.com>
2024-07-27 16:36:36 +0100

    Implement signup feature with access token saved in server side session

--- frontend/src/app/lib/actions.ts
+++ frontend/src/app/lib/actions.ts
@@ -1,6 +1,7 @@
 "use server";
 import { cookies } from "next/headers";
 
+// sets cookies when login
 export async function handleLogin(userId: string, accessToken: string, refreshToken: string) {
     cookies().set('session_userId', userId, {
         httpOnly: true,
